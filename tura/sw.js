ce4fc804287ba1e00bbf5818e90d1b0488dc5219
sw3gmach1n3 <belualexandru2006@gmail.com>
2025-08-15 15:30:53 +0300

    Delete tura directory

--- tura/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-self.addEventListener('fetch', event => {
-  // network-first for dynamic content; minimal offline fallback
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
